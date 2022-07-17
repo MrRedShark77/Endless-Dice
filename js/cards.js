046d341414c5731d12310780b49947ca9bed999e
MrRedShark77 <mrredshark77@gmail.com>
2022-07-17 09:54:58 +0300

    public beta

---
+++ js/cards.js
@@ -0,0 +1,179 @@
+const CARDS = {
+    d1: [
+        "Side Increaser",
+        x=>`Increase ${['your',"enemy's"][x]} maximum number of side by <b class='green'>1</b>`,
+        x=>data.round <= 10,
+        x=>{
+            data[x].max_s += 1
+        },
+    ],
+    d2: [
+        "Side Increaser",
+        x=>`Increase ${['your',"enemy's"][x]} maximum number of side by <b class='green'>2</b>`,
+        x=>true,
+        x=>{
+            data[x].max_s += 2
+        },
+    ],
+    d3: [
+        "Minimum Side Increaser",
+        x=>`Increase ${['your',"enemy's"][x]} minimum number of side by <b class='green'>1</b>`,
+        x=>data[x].min_s<data[x].max_s && data.round <= 10,
+        x=>{
+            data[x].min_s += 1
+        },
+    ],
+    d4: [
+        "Side Translation",
+        x=>`Increase ${['your',"enemy's"][x]} minimum & maximum number of side by <b class='green'>1</b>`,
+        x=>true,
+        x=>{
+            data[x].min_s += 1
+            data[x].max_s += 1
+        },
+    ],
+    d5: [
+        "Side Re-Increaser",
+        x=>`Increase ${['your',"enemy's"][x]} maximum number of side by <b class='green'>4</b>`,
+        x=> data.round > 10,
+        x=>{
+            data[x].max_s += 4
+        },
+    ],
+    d6: [
+        "Side Expansion",
+        x=>`Increase ${['your',"enemy's"][x]} minimum & maximum number of side by <b class='green'>2</b>`,
+        x=>true,
+        x=>{
+            data[x].min_s += 2
+            data[x].max_s += 2
+        },
+    ],
+
+    s1: [
+        "Sacrifice for Multiplier",
+        x=>`Sacrifice <b class='green'>80%</b> of your health for increasing the multiplier of product by <b class='green'>1</b>`,
+        x=>x=="player" && data.round <= 20,
+        x=>{
+            data[x].health = Math.ceil(data[x].health*0.2)
+            data[x].mult += 1
+        },
+    ],
+    s2: [
+        "Sacrifice for Multiplier",
+        x=>`Sacrifice <b class='green'>95%</b> of your health for increasing the multiplier of product by <b class='green'>3</b>`,
+        x=>x=="player" && data.round > 20,
+        x=>{
+            data[x].health = Math.ceil(data[x].health*0.05)
+            data[x].mult += 3
+        },
+    ],
+
+    e1: [
+        "Energy Increaser",
+        x=>`Increase ${['your',"enemy's"][x]} maximum energy by <b class='green'>3</b>`,
+        x=>true,
+        x=>{
+            data[x].maxEnergy += 3
+        },
+    ],
+    e2: [
+        "Energy Increaser",
+        x=>`Increase ${['your',"enemy's"][x]} maximum energy by <b class='green'>5</b>`,
+        x=>true,
+        x=>{
+            data[x].maxEnergy += 5
+        },
+    ],
+    e3: [
+        "Free Energy",
+        x=>`Consuming ${['your',"enemy's"][x]} energy has <b class='green'>20%</b> chance to get <b class='green'>2</b> free energy`,
+        x=>!data[x].cards.includes("e3"),
+        x=>{},
+    ],
+    
+    en1: [
+        "Enemy's Oktoberfest",
+        x=>`Increase enemy's starting health by <b class='green'>50%</b>`,
+        x=>x=="enemy",
+        x=>{
+            data.enemy.maxHealth = Math.floor(data.enemy.maxHealth*1.5)
+        },
+    ],
+    en2: [
+        "Stronger Enemy",
+        x=>`Increase enemy's multiplier by <b class='green'>1</b>`,
+        x=>x=="enemy" && data.round <= 10,
+        x=>{
+            data.enemy.mult += 1
+        },
+    ],
+    en3: [
+        "Mega Enemy",
+        x=>`Increase enemy's multiplier by <b class='green'>2</b>`,
+        x=>x=="enemy" && data.round > 10,
+        x=>{
+            data.enemy.mult += 2
+        },
+    ],
+    en4: [
+        "Catastrophic",
+        x=>`Increase enemy's multiplier by <b class='green'>4</b>`,
+        x=>x=="enemy" && data.round > 20,
+        x=>{
+            data.enemy.mult += 4
+        },
+    ],
+    en5: [
+        "Giant Enemy",
+        x=>`Increase enemy's starting health by <b class='green'>100%</b>`,
+        x=>x=="enemy" && data.round > 10,
+        x=>{
+            data.enemy.maxHealth = Math.floor(data.enemy.maxHealth*2)
+        },
+    ],
+
+    m1: [
+        "Multiplier Increaser",
+        x=>`Increase ${['your',"enemy's"][x]} multiplier by <b class='green'>0.25</b>`,
+        x=>true,
+        x=>{
+            data[x].mult += 0.25
+        },
+    ],
+    m2: [
+        "Multiplier Expansion",
+        x=>`Increase ${['your',"enemy's"][x]} multiplier by <b class='green'>0.75</b>`,
+        x=>data.round > 10,
+        x=>{
+            data[x].mult += 0.75
+        },
+    ],
+
+    o1: [
+        "Cleaner",
+        x=>`Clear all of your dices`,
+        x=>x=="player",
+        x=>{
+            data.p_grid = {}
+        },
+    ],
+
+    curse1: [
+        "Cursed Multiplier",
+        x=>`Multiply your number of side by <b class='green'>2</b>, but divide your multiplier by <b class='red'>2</b>`,
+        x=>x=="player"&&Math.random()<1/4,
+        x=>{
+            data[x].min_s *= 2
+            data[x].max_s *= 2
+
+            data[x].mult /= 2
+        },
+    ],
+    curse2: [
+        "Cursed Heart",
+        x=>`If you pass a round, will increase your health by <b class='green'>5%</b>, but increase enemy's multiplier by <b class='red'>5%</b> for passing it`,
+        x=>x=="player" && !data[x].cards.includes("curse2") && Math.random()<1/4,
+        x=>{},
+    ],
+}
---
+++ js/main.js
@@ -0,0 +1,409 @@
+var data = {
+    p_grid: {},
+    e_grid: {},
+
+    move: "player",
+    enemyReady: false,
+    end: false,
+
+    round: 1,
+
+    player: {
+        pick: [0,0],
+        pickStep: 0,
+        product: 1,
+        energyCos: 0,
+        health: 100,
+        energy: 10,
+        maxEnergy: 10,
+
+        mult: 1,
+
+        min_s: 1,
+        max_s: 6,
+
+        cards: [],
+    },
+
+    enemy: {
+        pick: [0,0],
+        product: 1,
+        energyCos: 0,
+        health: 100,
+        maxHealth: 100,
+        energy: 0,
+        maxEnergy: 10,
+
+        mult: 1,
+
+        min_s: 1,
+        max_s: 6,
+
+        cards: [],
+    },
+}
+
+var tmp = {
+    av_p_slots: [11,12,13,14,15,21,22,23,24,25,31,32,33,34,35,41,42,43,44,45],
+    av_e_slots: [11,12,13,14,15,21,22,23,24,25,31,32,33,34,35,41,42,43,44,45],
+}
+
+const stringToString = {
+    p_grid: ["player","av_p_slots","p_dice"],
+    e_grid: ["enemy","av_e_slots","e_dice"],
+    player: ["p_grid","enemy"],
+    enemy: ["e_grid","player"],
+}
+
+function randomInt(min,max) { return Math.floor(Math.random() * (max + 1 - min) + min) }
+
+function shuffle(array) {
+    let currentIndex = array.length,  randomIndex;
+    while (currentIndex != 0) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex], array[currentIndex]];
+    }
+  
+    return array;
+}
+
+function nextRound() {
+    data.round++
+    data.enemy.maxHealth = Math.floor(data.enemy.maxHealth*(data.round>20?1.5:1.2))
+    data.enemy.health = data.enemy.maxHealth
+    data.e_grid = {}
+    
+    resetTwo("player")
+    resetTwo("enemy")
+
+    data.move = "player"
+
+    document.getElementById("win_div").style.display = "none"
+    data.end = false
+
+    updateHTML()
+    updateAvSlots("p_grid")
+    updateAvSlots("e_grid")
+    updateGridDices("p_grid")
+    updateGridDices("e_grid")
+}
+
+function chooseCard(p,e) {
+    data.player.cards.push(p)
+    data.enemy.cards.push(e)
+
+    CARDS[p][3]("player")
+    CARDS[e][3]("enemy")
+
+    nextRound()
+}
+
+function generateRandomCards() {
+    var t = ["player","enemy"]
+    var ac = [[],[]]
+    var cs = document.getElementById("pick_cards")
+    cs.innerHTML = ""
+    
+    for (i in t) {
+        var ks = Object.keys(CARDS)
+
+        for (j in ks) if (CARDS[ks[j]][2](t[i])) ac[i].push(ks[j])
+    }
+
+    for (let i = 0; i < 3; i++) {
+        let [p,e] = [ac[0][Math.floor(Math.random()*ac[0].length)],ac[1][Math.floor(Math.random()*ac[1].length)]]
+        let [cp,ce] = [CARDS[p],CARDS[e]]
+
+        cs.innerHTML += `
+        <div class="card table_center" onclick="chooseCard('${p}','${e}')">
+            <div class="card_p">
+                ${cp[0]}<br>
+                <span style="font-size: 18px">${cp[1](0)}</span>
+            </div>
+            <div class="card_e">
+                ${ce[0]}<br>
+                <span style="font-size: 18px">${ce[1](1)}</span>
+            </div>
+        </div>
+        `
+    }
+}
+
+function pickDice(pos) {
+    let g = data.p_grid[pos]
+
+    if (!g || data.player.pick.includes(pos) || data.player.pickStep > 1 || data.end) return
+
+    data.player.pick[data.player.pickStep] = pos
+    data.player.energyCos += g.energy
+    data.player.pickStep++
+    data.player.product *= g.value
+
+    updateGridDices('p_grid')
+}
+
+function makeMove(move="player") {
+    var d = data[move]
+    var g = data[stringToString[move][0]]
+    var od = data[stringToString[move][1]]
+
+    if ((d.pickStep > 1 || move == "enemy") && d.energy >= d.energyCos) {
+        d.energy -= d.energyCos
+
+        if (d.cards.includes("e3") && Math.random() < .2) d.energy += 2
+
+        var p = Math.floor(d.product*d.mult)
+        if (Math.random() < .15) p *= 2
+        var dices = [g[d.pick[0]],g[d.pick[1]]]
+
+        g[d.pick[0]] = undefined
+        g[d.pick[1]] = undefined
+
+        for (x in dices) {
+            var dice = dices[x]
+
+            if (dice.type == "attack") od.health = Math.max(od.health-p,0)
+            else if (dice.type == "heal") d.health += p
+        }
+
+        if (dices[0].type == dices[1].type) {
+            var dice = dices[0]
+            if (dice.type == "attack") od.health = Math.max(od.health-p,0)
+            else if (dice.type == "heal") d.health += p
+        }
+        
+        d.energyCos = 0
+        d.pick = [0,0]
+        d.pickStep = 0
+        d.product = 1
+
+        updateGridDices("p_grid")
+        updateGridDices("e_grid")
+
+        updateAvSlots("p_grid")
+        updateAvSlots("e_grid")
+
+        if (od.health <= 0) {
+            data.end = true
+            setTimeout(conclusion,1000)
+        }
+    }
+}
+
+function pass() {
+    if (data.move == "player" && !data.end) {
+        data.move = "enemy"
+        data.enemy.energy = data.enemy.maxEnergy
+
+        resetOne("player")
+
+        setTimeout(autoEnemyMove,1000)
+    }
+}
+
+function resetOne(id) {
+    data[id].energy = 0
+    data[id].energyCos = 0
+    data[id].pick = [0,0]
+    data[id].pickStep = 0
+    data[id].product = 1
+
+    updateGridDices("p_grid")
+    updateGridDices("e_grid")
+}
+
+function resetTwo(id) {
+    data[id].energy = data[id].maxEnergy
+    data[id].energyCos = 0
+    data[id].pick = [0,0]
+    data[id].pickStep = 0
+    data[id].product = 1
+}
+
+function conclusion() {
+    var c = document.getElementById("conclusion")
+
+    if (data.player.health <= 0) {
+        c.innerHTML = "You lose!"
+    } else if (data.enemy.health <= 0) {
+        generateRandomCards()
+        document.getElementById("win_div").style.display = "block"
+    }
+
+    updateHTML()
+}
+
+function updateHTML() {
+    document.getElementById("conclusion").style.display = data.end ? "block" : "none"
+}
+
+function autoEnemyMove() {
+    if (data.enemyReady) {
+        makeMove("enemy")
+
+        data.enemyReady = false
+    } else {
+        var hd = []
+        var gd = []
+        var g = Object.keys(data.e_grid)
+        var de = data.enemy, trueNoNormal = tmp.av_e_slots.length == 0
+        for (let i in g) if (data.e_grid[g[i]]) {
+            if (!trueNoNormal && data.e_grid[g[i]].type != "normal") trueNoNormal = true
+            gd.push(data.e_grid[g[i]])
+            hd.push(g[i])
+        }
+
+        var rgd = []
+        if (hd.length >= 2 && trueNoNormal) {
+            step = 1
+            noCost = true
+            rgd = shuffle(gd).slice(0,2)
+
+            while ((rgd[0].type == "normal" && rgd[1].type == "normal" || tmp.av_e_slots.length == 0) && de.energy >= rgd[0].energy + rgd[1].energy) {
+                rgd = shuffle(gd).slice(0,2)
+
+                console.log(rgd[0].type, rgd[1].type)
+
+                step++
+                if (step >= 100) break
+            }
+        }
+
+        if (de.energy > 0) {
+            if (rgd.length > 0 ? hd.length >= 2 && de.energy >= rgd[0].energy + rgd[1].energy && trueNoNormal && (Math.random() < 0.75 || tmp.av_e_slots.length == 0) : false) {
+                data.enemyReady = true
+
+                de.pick = [rgd[0].pos,rgd[1].pos]
+                de.energyCos = rgd[0].energy + rgd[1].energy
+                de.product = rgd[0].value * rgd[1].value
+            } else spawnCost("e_grid")
+        } else {
+            data.move = "player"
+            data.player.energy = data.player.maxEnergy
+    
+            resetOne("enemy")
+    
+            return
+        }
+    }
+
+    updateGridDices("p_grid")
+    updateGridDices("e_grid")
+
+    if (!data.end) setTimeout(autoEnemyMove,1000)
+}
+
+function updateAvSlots(id) {
+    var a = []
+    var d = data[id]
+    for (let y = 1; y <= 4; y++) for (let x = 1; x <= 5; x++) if (!d[y*10+x]) a.push(y*10+x)
+    tmp[stringToString[id][1]] = a
+}
+
+function createGridDices(id) {
+    var grid = document.getElementById(id);
+    var html = ""
+    for (let y = 1; y <= 4; y++) for (let x = 1; x <= 5; x++) html += `<div class="grid-dice empty" id="${id}_${y}${x}" ${id != "e_grid" ? `onclick="pickDice(${y}${x})"` : ""}></div>`
+    grid.innerHTML = html
+
+    updateGridDices(id)
+}
+
+function clearPick() {
+    data.player.energyCos = 0
+    data.player.pick = [0,0]
+    data.player.pickStep = 0
+    data.player.product = 1
+
+    updateGridDices("p_grid")
+}
+
+function updateGridDices(id) {
+    var grid = data[id]
+    var dd = stringToString[id][0]
+    var d = data[dd]
+
+    document.getElementById(dd+"_move").innerHTML = data.move == dd ? " (Move)" : ""
+
+    if (dd == "player") {
+        document.getElementById("player_proccess").style.display = data.player.pickStep > 1 && data.player.energy >= data.player.energyCos ? "inline" : "none"
+        document.getElementById("player_proccess").innerHTML = `Make a Move, ${data.player.energyCos} Energy Cost`
+
+        document.getElementById("player_clear").style.display = data.player.pickStep > 0 ? "inline" : "none"
+        document.getElementById("player_pass").style.display = data.move == "player" ? "inline" : "none"
+    }
+
+    document.getElementById(dd+"_health").innerHTML = "Health: " + format(d.health)
+    document.getElementById(dd+"_energy").innerHTML = format(d.energy) + " / " + format(d.maxEnergy)
+    document.getElementById(dd+"_mult").innerHTML = d.mult.toFixed(2)
+    document.getElementById(dd+"_result").innerHTML = (d.pickStep > 1 || (id=="e_grid" && data.enemyReady)) ? format(Math.floor(d.product*d.mult)) : "?"
+
+
+    for (let y = 1; y <= 4; y++) for (let x = 1; x <= 5; x++) {
+        var i = y*10+x
+        var g = document.getElementById(id+"_"+i)
+        var gd = grid[i]
+
+        g.innerHTML = "<div>"+(gd ? (gd.value > 9 ? gd.value : "") + `<span class="energy">${gd.energy}</span>` : "")+"</div>"
+        g.className = "grid-dice "+(gd ? gd.type : "empty")
+
+        if (d.pick.includes(i)) g.className += " picked"
+
+        if (gd) {
+            if (gd.type != "normal") g.innerHTML += `<img draggable="false" src='images/${gd.type}.png'>`
+            if (gd.value < 10) g.innerHTML += `<img draggable="false" src='images/D${gd.value}.png'>`
+        }
+    }
+
+    for (let i = 0; i < 2; i++) {
+        var pdiv = document.getElementById(stringToString[id][2]+i)
+        var pd = d.pick[i]
+
+        pdiv.innerHTML = ""
+        pdiv.className = "picked_dice" 
+
+        if (pd > 0) {
+            var gd = grid[pd]
+
+            pdiv.innerHTML = "<div>"+(gd ? gd.value > 9 ? gd.value : "" : "")+"</div>"
+            pdiv.className = "picked_dice "+(gd ? gd.type : "empty")
+
+            if (gd.type != "normal") pdiv.innerHTML += `<img draggable="false" src='images/${gd.type}.png'>`
+            if (gd.value < 10) pdiv.innerHTML += `<img draggable="false" src='images/D${gd.value}.png'>`
+        }
+    }
+}
+
+function spawnCost(id) {
+    var d = data[stringToString[id][0]]
+
+    if (d.energy - 1 >= 0 && tmp[stringToString[id][1]].length > 0 && (id == "p_grid" ? !data.end : true)) {
+        d.energy -= 1
+
+        spawnRandomDice(id,true)
+    }
+}
+
+function spawnRandomDice(id,update=false) {
+    var grid = data[id]
+    var d = data[stringToString[id][0]]
+    var s = tmp[stringToString[id][1]]
+    var pos = s[Math.floor(Math.random()*s.length)]
+    var tp = Math.floor(Math.random()*3)
+
+    grid[pos] = {pos: pos, value: randomInt(d.min_s,d.max_s), type: ["normal","attack","heal"][tp], energy: [1,2,2][tp]}
+
+    updateAvSlots(id)
+    if (update) updateGridDices(id)
+}
+
+function format(x) {
+    return x.toFixed(0)
+}
+
+function loadGame() {
+    createGridDices("p_grid")
+    createGridDices("e_grid")
+}
